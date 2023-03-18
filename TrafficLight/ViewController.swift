6bae15623fbbdc249703ccf8167a377e9bdf57d8
Пахаев Хусейн <pakhaev01@gmail.com>
2023-03-19 01:00:40 +0300

    Added work logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var trafficLightViews: [UIView]!
+    @IBOutlet var changeTrafficButton: UIButton!
+    
+    private var indexArray = 0
+    private var previousIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for trafficLight in trafficLightViews {
+            trafficLight.layer.cornerRadius = trafficLight.frame.size.width / 2
+            trafficLight.alpha = 0.3
+        }
+
+        changeTrafficButton.layer.cornerRadius = changeTrafficButton.bounds.height / 2
+        
     }
 
-
+    @IBAction func changeTrafficTapped() {
+        trafficLightViews[indexArray].alpha = 1
+        
+        if changeTrafficButton.currentTitle == "START" {
+            changeTrafficButton.setTitle("NEXT", for: .normal)
+        } else {
+            trafficLightViews[previousIndex].alpha = 0.3
+        }
+        
+        previousIndex = indexArray
+        indexArray = indexArray + 1 < trafficLightViews.count ? indexArray + 1 : 0
+    }
+    
 }
 
